557f6deacfbacb03dde51df177ef5a354e1c76d7
Jordan Thompson (WASasquatch) <jordanslost@gmail.com>
2017-03-07 11:37:55 -0800

    Update to v0.1-2
    Added World Support

--- src/wa/was/bg/events/BloodEffect.java
+++ src/wa/was/bg/events/BloodEffect.java
@@ -1,5 +1,7 @@
 package wa.was.bg.events;
 
+import java.util.List;
+
 import org.bukkit.Effect;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -11,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BloodEffect implements Listener {
 	
 	private JavaPlugin plugin;
+	private boolean isValidWorld = false;
 	
 	public BloodEffect(JavaPlugin plugin) {
 		this.plugin = plugin;
@@ -21,11 +24,18 @@ public class BloodEffect implements Listener {
 		try {
 			if ( e.isCancelled() ) return;
 			if ( e.getEntity().isValid() ) {
+				List<String> validWorlds = plugin.getConfig().getStringList("valid-worlds");
+				for ( String world : validWorlds ) {
+					if ( e.getEntity().getWorld().getName() == world )
+						isValidWorld = true;
+				}
+				if ( ! ( isValidWorld ) ) return;
 				for( String tag : plugin.getConfig().getConfigurationSection("Entities").getKeys(false) ) {
 					if ( EntityType.valueOf(tag.toUpperCase()) != null 
 							&& Effect.valueOf(plugin.getConfig().getString("Entities."+tag+".type")) != null ) {
+						// Entity Gore Effect
 						EntityType entity = EntityType.valueOf(tag.toUpperCase());
-						Effect type = Effect.valueOf(plugin.getConfig().getString("Entities."+tag+".type"));
+						Effect type = Effect.valueOf(plugin.getConfig().getString("Entities."+tag+".type").toUpperCase());
 						if ( e.getEntity().getType() == entity ) {
 							e.getEntity().getWorld().playEffect(e.getEntity().getLocation(), type, 
 									plugin.getConfig().getInt("Entities."+tag+".id")); 
@@ -34,7 +44,7 @@ public class BloodEffect implements Listener {
 					}
 				}
 				// Default gore type
-				Effect type = Effect.valueOf(plugin.getConfig().getString("default-type"));
+				Effect type = Effect.valueOf(plugin.getConfig().getString("default-type").toUpperCase());
 				e.getEntity().getWorld().playEffect(e.getEntity().getLocation(), type, 
 						plugin.getConfig().getInt("default-type")); 
 				return;
